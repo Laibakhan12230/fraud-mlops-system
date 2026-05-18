42cef00c3ba924fe388061cdd07376ea74a3b898
Laiba Khan <tubanaaz8901@gmail.com>
2026-05-19 02:31:32 +0530

    fixed mongo atlas env environment

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,14 +1,12 @@
 from pymongo import MongoClient
 import os
 
-client = MongoClient(
-    os.getenv("mongodb+srv://Laiba_khan:Laibanaaz1234@cluster0.839rxof.mongodb.net/?appName=Cluster0")
-)
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = MongoClient(MONGO_URL)
 
 db = client["fraud_detection"]
 
 prediction_collection = db["predictions"]
-
 users_collection = db["users"]
-
 history_collection = db["history"]
