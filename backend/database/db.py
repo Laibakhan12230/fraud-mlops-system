047ad3be73c97d11a6f6439e23e6c65d8448e488
Laiba Khan <tubanaaz8901@gmail.com>
2026-05-20 20:01:56 +0530

    Fixed MongoDB Atlas connection

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,12 +1,16 @@
 from pymongo import MongoClient
 import os
 
-MONGO_URL = os.getenv("MONGO_URL")
+MONGO_URL = os.environ.get("MONGO_URL")
+
+print("MONGO URL:", MONGO_URL)
 
 client = MongoClient(MONGO_URL)
 
 db = client["fraud_detection"]
 
 prediction_collection = db["predictions"]
+
 users_collection = db["users"]
+
 history_collection = db["history"]
